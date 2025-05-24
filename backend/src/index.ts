59c6f46a6af7ade6b9b42b8194b6ecd58d7f5ce5
Sayam Patel <sayamparejiya@gmail.com>
2025-05-24 16:53:10 +0530

    added the code of streaming response from model

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,11 +8,18 @@ dotenv.config({
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function main() {
-  const response = await ai.models.generateContent({
+
+  const response = await ai.models.generateContentStream({
     model: "gemini-2.0-flash",
-    contents: "what is 2+2",
+    contents: "create todo app in nodejs",
   });
-  console.log(response.text);
+
+  let text = "";
+  for await (const chunk of response) {
+    const part = chunk.text;
+    console.log(part);
+    text += part;
+  }
 }
  
 main();
