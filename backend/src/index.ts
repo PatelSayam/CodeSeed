34a11beb6871f3b74a99e1792820121c05074c91
Sayam Patel <sayamparejiya@gmail.com>
2025-05-25 15:35:38 +0530

    configure the chat and template route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,23 @@
 import dotenv from "dotenv";
-import { GoogleGenAI } from "@google/genai";
+import express from "express";
+import cors from 'cors';
+
+import chatRoutes from './routes/chat.ts';
+import templateRoutes from './routes/template.ts'
 
 dotenv.config({
-    path: './.env'
+  path: "./.env",
 });
 
-const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-async function main() {
 
-  const response = await ai.models.generateContentStream({
-    model: "gemini-2.0-flash",
-    contents: "create todo app in nodejs",
-  });
+app.use('/chat', chatRoutes);
+app.use('/template', templateRoutes);
 
-  let text = "";
-  for await (const chunk of response) {
-    const part = chunk.text;
-    console.log(part);
-    text += part;
-  }
-}
- 
-main();
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+})
+
